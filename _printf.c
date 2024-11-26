def176ac65fb8a773a053aaa6638967a153ecc9b
nyacho04 <dnachod83@gmail.com>
2024-11-26 10:40:14 -0300

    updated _printf function to handle additional format specifiers and added null check for string arguments

--- _printf.c
+++ _printf.c
@@ -2,52 +2,61 @@
 #include "_main.h"
 #include <unistd.h>
 /**
-* _printf - Emulates the printf function using write.
-* @format: The format string containing the text and specifiers.
-*
-* Return: The number of characters printed.
-*/
+ * _printf - Emulates the printf function using write.
+ * @format: The format string containing the text and specifiers.
+ *
+ * Return: The number of characters printed.
+ */
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
+    int count = 0;
+    va_list args;
 
-	va_list args;
+    if (format == NULL)
+        return (-1);
 
-	if (format == NULL)
-		return (-1);
+    va_start(args, format);
 
-	va_start(args, format);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                int c = va_arg(args, int);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				int c = va_arg(args, int);
+                _print(c);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *s = va_arg(args, char *);
+                if (s == NULL)
+                    s = "(null)";
+                _printstr(s);
 
-				_print(c);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
+            }
+            else if (*format == '%')
+            {
+                _print('%');
+                count++;
+            }
+            else
+            {
+                _print('%');
+                _print(*format);
+            }
+        }
+        else
+        {
+            _print(*format);
+            count++;
+        }
+        format++;
+    }
 
-				if (s == NULL)
-					s = "(null)";
-				_printstr(s);
-			}
-			else if (*format == '%')
-			{
-				_print('%');
-				count++;
-			}
-		}
-
-	va_end(args);
-	return (count);
-
-	}
+    va_end(args);
+    return (count);
 }
