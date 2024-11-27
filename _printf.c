8b3bf9e48a099a6ee587a79cf8dc8dd30cec9456
Rodrigo <rodrigoferre30@gmail.com>
2024-11-27 17:43:56 +0000

    Betty_style_finished

--- _printf.c
+++ _printf.c
@@ -3,41 +3,40 @@
 #include <unistd.h>
 #include "_main.h"
 /**
-* _printf - Function that recreates the function printf
-* @format: Type of data to print
-* Return: The count of characters impressed
-*/
+ * _printf - Function that recreates the function printf
+ * @format: Type of data to print
+ * Return: The count of characters impressed
+ */
 int _printf(const char *format, ...)
 {
-int count = 0;
-char *str;
-va_list args;
-va_start(args, format);
-if (format == NULL)
-return (-1);
+	int count = 0;
+	char *str;
+	va_list args;
+
+	va_start(args, format);
+	if (format == NULL || *format == '\0')
+		return (-1);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format == '\0')
-				return (-1);
 			switch (*format)
 			{
-			case 'c':
-				_printchar(va_arg(args, int));
-				count++;
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				count += _printstr(str);
-				break;
-			case '%':
-				_printchar('%');
-				count++;
-				break;
+				case 'c':
+					_printchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+					count += _printstr(str);
+					break;
+				case '%':
+					_printchar('%');
+					count++;
+					break;
 			}
 		}
 		else
