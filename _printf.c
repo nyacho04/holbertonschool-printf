ec75d4327fea0a946fb050d954e50035935192a2
nyacho04 <dnachod83@gmail.com>
2024-11-27 14:24:51 -0300

    "Updated _printf function implementation

--- _printf.c
+++ _printf.c
@@ -3,54 +3,57 @@
 #include <unistd.h>
 #include "_main.h"
 /**
- * _printf - Function that recreates the function printf
- * @format: Type of data to print
- * Return: The count of characters impressed
- */
+* _printf - Function that recreates the function printf
+* @format: Type of data to print
+* Return: The count of characters impressed
+*/
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    char *str;
-    va_list args;
+	int count = 0;
 
-    if (format == NULL)
-        return (-1);
+	char *str;
 
-    va_start(args, format);
+	va_list args;
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '\0') 
-                return (-1);
+	if (format == NULL)
+		return (-1);
 
-            switch (*format)
-            {
-            case 'c':
-                _printchar(va_arg(args, int));
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				return (-1);
+
+			switch (*format)
+			{
+			case 'c':
+				_printchar(va_arg(args, int));
+				count++;
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				count += _printstr(str);
+				break;
+            case '%':
+                _printchar('%');
                 count++;
                 break;
-            case 's':
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                count += _printstr(str);
-                break;
-            default:
-                _printchar(*format);
-                count ++;
-            }
-        }
+			}
+		}
         else
         {
             _printchar(*format);
             count++;
         }
-        format++;
-    }
+		format++;
+	}
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
